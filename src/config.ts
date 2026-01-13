ef451562c0eba8c5ec98a8241445c2d1ae356d28
bekzodnj <bekzodnx@gmail.com>
2026-01-13 23:09:35 +0100

    favicon changes and navbar styling

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE_LOGO = "/avatar.jpg";
-export const SITE_TITLE = "Bekzod Jon ";
+export const SITE_TITLE = "Bekzod Jon";
 export const SITE_DESCRIPTION =
   "Bekzodjon Norkuziev's personal blog about web development, programming, and technology.";
 
@@ -8,16 +8,19 @@ export const MENUS = [
     title: "Posts",
     link: "/posts",
     target: "_self",
+    emoji: "",
   },
   {
     title: "Projects",
     link: "/projects",
     target: "_self",
+    emoji: "🥬",
   },
   {
     title: "Tags",
     link: "/tags",
     target: "_self",
+    emoji: "",
   },
   // {
   //   title: "Archive",
@@ -25,19 +28,22 @@ export const MENUS = [
   //   target: "_self",
   // },
   {
-    title: "About Me",
+    title: "About",
     link: "/about",
     target: "_self",
+    emoji: "",
   },
   {
     title: "GitHub ↗",
     link: "https://github.com/bekzodnj",
     target: "_blank",
+    emoji: "",
   },
   {
     title: "Twitter / X ↗",
     link: "https://x.com/bekzodnj",
     target: "_blank",
+    emoji: "",
   },
 ];
 
